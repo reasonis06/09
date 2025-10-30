5e8f9ded22f528a32135cddbec2af2ed63d8f6e4
이성은 <katrina060208@gmail.com>
2025-10-30 15:39:27 +0900

    실습2

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5
 
 int main(void)
 {
 	int i;
-  int grade[5];
-  
-  grade[0] = 10;
-  grade[1] = 20;
-  grade[2] = 30;
-  grade[3] = 40;
-  grade[4] = 50;
-  
-  for(i=0; i<5; i++)
-  	printf("grade[%d] = %d\n", i, grade[i]);
-  	
-  system("PAUSE");	
-  return 0;
+	int average;
+	int score[SIZE];
+	
+	int sum = 0;
+	for(i=0; i<SIZE; i++){
+		printf("Enter a score : ");
+		scanf("%d", &score[i]);
+		sum += score[i];
+	}
+	
+	average = sum / SIZE;
+	
+	printf("average score : %d\n", average);
+	
+	system("PAUSE");
+	return 0;
 }
