314034de5c3b39c8df160a9025e3a71f03056517
이성은 <katrina060208@gmail.com>
2025-10-30 16:05:08 +0900

    실습3

--- main.c
+++ main.c
@@ -2,23 +2,42 @@
 #include <stdlib.h>
 #define SIZE 5
 
+#if 0
 int main(void)
 {
 	int i;
-	int average;
+	int grade[SIZE];
 	int score[SIZE];
 	
-	int sum = 0;
-	for(i=0; i<SIZE; i++){
-		printf("Enter a score : ");
-		scanf("%d", &score[i]);
-		sum += score[i];
+	for(i=0; i<SIZE; i++) {
+		grade[i] = rand() % 100; // random number's range: 0~99
+		score[i] = grade[i];
 	}
 	
-	average = sum / SIZE;
+	for(i=0; i<SIZE; i++) {
+		printf("score[%d] = %d\n", i, score[i]);
+		printf("grade[%d] = %d\n", i, score[i]);
+	}
 	
-	printf("average score : %d\n", average);
+	system("PAUSE");
+	return 0;
+}
+#endif
+int main(void){
+	int i;
+	int a[SIZE] = {1,2,3,4,5};
+	int b[SIZE] = {1,2,3,4,5};
+	int flag_same = 1; // flag variable
 	
+	for(i=0; i<SIZE; i++) {
+		if(a[i] != b[i]) {
+			printf("%d-th element is different.\n", i);
+			flag_same = 0;
+		}
+	}
+
+printf("Are a and b the same? %d\n", flag_same);
+
 	system("PAUSE");
 	return 0;
 }
